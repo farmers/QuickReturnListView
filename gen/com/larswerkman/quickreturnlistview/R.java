16d023d87fb0ca35b814cec7df344d2513002ff0
amond lee <amond@amond.net>
2013-12-06 01:56:51 +0900

    supprot library update

--- gen/com/larswerkman/quickreturnlistview/R.java
+++ gen/com/larswerkman/quickreturnlistview/R.java
@@ -11,43 +11,43 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int sticky_item=0x7f040001;
-        public static final int sticky_item_text=0x7f040002;
-        public static final int top_item=0x7f040000;
+        public static int sticky_item=0x7f040001;
+        public static int sticky_item_text=0x7f040002;
+        public static int top_item=0x7f040000;
     }
     public static final class dimen {
-        public static final int page_margin=0x7f050002;
-        public static final int sticky_height=0x7f050000;
-        public static final int top_height=0x7f050001;
+        public static int page_margin=0x7f050002;
+        public static int sticky_height=0x7f050000;
+        public static int top_height=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int footer=0x7f080001;
-        public static final int pager=0x7f080000;
-        public static final int placeholder=0x7f080003;
-        public static final int sticky=0x7f080002;
-        public static final int text1=0x7f080004;
+        public static int footer=0x7f080001;
+        public static int pager=0x7f080000;
+        public static int placeholder=0x7f080003;
+        public static int sticky=0x7f080002;
+        public static int text1=0x7f080004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int footer_fragment=0x7f030001;
-        public static final int fragment=0x7f030002;
-        public static final int header=0x7f030003;
-        public static final int list_item=0x7f030004;
+        public static int activity_main=0x7f030000;
+        public static int footer_fragment=0x7f030001;
+        public static int fragment=0x7f030002;
+        public static int header=0x7f030003;
+        public static int list_item=0x7f030004;
     }
     public static final class string {
-        public static final int animation_fragment=0x7f060003;
-        public static final int app_name=0x7f060000;
-        public static final int default_fragment=0x7f060002;
-        public static final int footer=0x7f060004;
-        public static final int footer_fragment=0x7f060005;
-        public static final int title_activity_main=0x7f060001;
+        public static int animation_fragment=0x7f060003;
+        public static int app_name=0x7f060000;
+        public static int default_fragment=0x7f060002;
+        public static int footer=0x7f060004;
+        public static int footer_fragment=0x7f060005;
+        public static int title_activity_main=0x7f060001;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
-        public static final int Item=0x7f070001;
-        public static final int Item_Sticky=0x7f070002;
+        public static int AppTheme=0x7f070000;
+        public static int Item=0x7f070001;
+        public static int Item_Sticky=0x7f070002;
     }
 }
